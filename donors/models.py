8a340f576bec8d476143280d89ecda6af9beacd8
ejembi faith ehi <ehifaith54@gmail.com>
2026-09-10 11:55:31 -0400

    the reg dashboard

--- donors/models.py
+++ donors/models.py
@@ -6,6 +6,6 @@ class Donor(models.Model):
     genotype = models.CharField(max_length=2)
     location = models.CharField(max_length=200)
     phone = models.CharField(max_length=20)
-    last_donation = models.CharField(max_length=50)
+    last_donation = models.DateField(null=True, blank=True)
     availability = models.BooleanField(default=True)
 # Create your models here.
--- donors/views.py
+++ donors/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
+from .forms import DonorForm
 
 
 def register_donor(request):
-    return render(request, "donors/register.html")
+    if request.method == "POST":
+        form = DonorForm(request.POST)
 
-# Create your views here.
+        if form.is_valid():
+            form.save()
+
+            return render(request, "donors/register.html", {
+                "form": DonorForm(),
+                "success": True
+            })
+
+    else:
+        form = DonorForm()
+
+    return render(request, "donors/register.html", {"form": form})
